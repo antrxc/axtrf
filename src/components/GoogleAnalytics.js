0b6ca6eb9e3564f373b79871ccdc21df17b286f5
Antric K Nayakam <132219079+antrxc@users.noreply.github.com>
2024-10-07 10:53:00 +0530

    Changed env

--- src/components/GoogleAnalytics.js
+++ src/components/GoogleAnalytics.js
@@ -4,7 +4,7 @@ import ReactGA from 'react-ga';
 
 const GoogleAnalytics = () => {
     useEffect(() => {
-        ReactGA.initialize('9777726267');
+        ReactGA.initialize('G-Q41P747MJ6');
     }, []);
 
     return null;
